5a9697038c6f93a7ac12d677548aeef293247b6b
nuyhank@gmail.com <nuyhank@gmail.com>
2025-04-14 16:01:14 +0900

    changing file names

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "../styles/index.css";
+import "../styles/Login.css";
 import "../styles/Register.css";
 import "../styles/AuthButtons.css";
 

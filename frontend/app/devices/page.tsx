b21e51aeaadb6adea927459c69e232e75db5f063
Marc Owen <marcowen20@gmail.com>
2025-06-13 14:05:41 +0900

    Add link to install script

--- frontend/app/devices/page.tsx
+++ frontend/app/devices/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Account, Models } from 'appwrite';
-import MainLayout from '@/components/MainLayout';
-import { Input } from '@/components/ui/input';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Account, Models } from "appwrite";
+import MainLayout from "@/components/MainLayout";
+import { Input } from "@/components/ui/input";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { Check, Copy, Plus } from 'lucide-react';
+import { Check, Copy, Plus } from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -16,13 +16,13 @@ import {
   DialogTrigger,
   DialogFooter,
   DialogClose,
-} from '@/components/ui/dialog';
-import { getAppwriteClient, getCurrentUser } from '@/lib/appwrite';
-import { backendFetch, cn } from '@/lib/utils';
-import { toast } from 'sonner';
-import { getDeviceColumns } from '../dashboard/columns';
-import { DataTable } from '@/components/ui/data-table';
-import { ReloadButton } from '@/components/common/ReloadButtion';
+} from "@/components/ui/dialog";
+import { getAppwriteClient, getCurrentUser } from "@/lib/appwrite";
+import { backendFetch, cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { getDeviceColumns } from "../dashboard/columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ReloadButton } from "@/components/common/ReloadButtion";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -48,9 +48,11 @@ function AddDeviceButton() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
 
-  const title ="Add New Device";
-  const description = "Run the following command on your new device to register it.";
-  const script = "curl -fsSL https://tailscale.com/install.sh | sh";
+  const title = "Add New Device";
+  const description =
+    "Run the following command on your new device to register it.";
+  const script =
+    "curl -fsSL https://github.com/SKKU-Capstone-Team-7/hallucinet/releases/latest/download/install.sh | sh";
 
   const handleCopy = useCallback(async () => {
     try {
@@ -65,15 +67,15 @@ function AddDeviceButton() {
       console.error("Copy to clipboard failed:", e);
     }
   }, [script]);
-  
+
   return (
     <Dialog
       open={isDialogOpen}
       onOpenChange={(open) => {
         setIsDialogOpen(open);
-        if (!open)
-          setIsCopied(false);
-      }}>
+        if (!open) setIsCopied(false);
+      }}
+    >
       <DialogTrigger asChild>
         <Button className="cursor-pointer w-30">
           <div className="flex items-center gap-1">
@@ -97,25 +99,25 @@ function AddDeviceButton() {
               </code>
               <Button
                 variant="ghost"
-                size="icon"     
+                size="icon"
                 onClick={handleCopy}
-                className="h-7 w-7 cursor-pointer" 
+                className="h-7 w-7 cursor-pointer"
                 aria-label="Copy DNS name"
               >
                 {isCopied ? (
                   <Check className="h-4 w-4" />
-                  ) : (
-                  <Copy className="h-4 w-4 cursor-pointer"/>
+                ) : (
+                  <Copy className="h-4 w-4 cursor-pointer" />
                 )}
               </Button>
             </div>
           </div>
         </div>
         <DialogFooter>
-          <DialogClose 
+          <DialogClose
             className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "cursor-pointer"
+              buttonVariants({ variant: "ghost" }),
+              "cursor-pointer",
             )}
           >
             Cancel
@@ -129,13 +131,15 @@ function AddDeviceButton() {
 export default function DevicesPage() {
   const [initialLoading, setInitialLoading] = useState(true);
   const [isDevicesLoading, setIsDevicesLoading] = useState<boolean>(false);
-  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null,
+  );
   const [devices, setDevices] = useState<DeviceInfo[]>([]);
   const router = useRouter();
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [selectedDevice, setSelectedDevice] = useState<DeviceInfo | null>(null);
-  const [editedName, setEditedName] = useState('');
+  const [editedName, setEditedName] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const loadDevices = useCallback(async () => {
@@ -143,13 +147,13 @@ export default function DevicesPage() {
       console.log("User not available, cannot load devices.");
       return;
     }
-    
+
     console.log("fetch device");
     setIsDevicesLoading(true);
     try {
       const account = new Account(getAppwriteClient());
       const jwt = (await account.createJWT()).jwt;
-    
+
       const devicesRes = await backendFetch("/teams/me/devices", "GET", jwt);
       if (!devicesRes.ok) {
         throw new Error(`Failed to fetch devices: ${devicesRes.statusText}`);
@@ -161,9 +165,9 @@ export default function DevicesPage() {
         subnet: dev["ipBlock24"],
         userEmail: dev["user"]["email"],
         last_activate: new Date(dev["lastActivatedAt"]),
-        userId: dev["user"]["$id"]
+        userId: dev["user"]["$id"],
       }));
-    
+
       setDevices(fetchedDevices);
     } catch (e) {
       console.error("Failed to load devices:", e);
@@ -191,7 +195,8 @@ export default function DevicesPage() {
 
         // Check if user is in a team
         const meRes = await backendFetch("/users/me", "GET", jwt);
-        if (!meRes.ok) throw new Error(`Initial user check failed: ${meRes.statusText}`);
+        if (!meRes.ok)
+          throw new Error(`Initial user check failed: ${meRes.statusText}`);
         const meJson = await meRes.json();
         const teams: string[] = meJson["teamIds"] || [];
         if (teams.length == 0) {
@@ -214,11 +219,11 @@ export default function DevicesPage() {
     }
   }, [user, initialLoading, loadDevices]);
 
-  const handleNameClick = (device : DeviceInfo) => {
+  const handleNameClick = (device: DeviceInfo) => {
     setSelectedDevice(device);
     setEditedName(device["name"]);
     setIsDialogOpen(true);
-  }
+  };
 
   const handleSave = async () => {
     if (!selectedDevice || !editedName.trim()) return;
@@ -227,9 +232,14 @@ export default function DevicesPage() {
       const account = new Account(getAppwriteClient());
       const jwt = (await account.createJWT()).jwt;
 
-      const deviceRes = await backendFetch(`/devices/${selectedDevice.id}`, 'PATCH', jwt, {
-        name: editedName.trim(),
-      });
+      const deviceRes = await backendFetch(
+        `/devices/${selectedDevice.id}`,
+        "PATCH",
+        jwt,
+        {
+          name: editedName.trim(),
+        },
+      );
       if (!deviceRes.ok) {
         const err = await deviceRes.json();
         throw new Error(err.message || "Failed to updtae device name.");
@@ -239,7 +249,7 @@ export default function DevicesPage() {
     } catch (e) {
       console.error("Error saving device name:", e);
       toast.error("Updated Failed", {
-        description: (e as Error).message 
+        description: (e as Error).message,
       });
     } finally {
       setIsSubmitting(false);
@@ -254,18 +264,22 @@ export default function DevicesPage() {
     try {
       const account = new Account(getAppwriteClient());
       const jwt = (await account.createJWT()).jwt;
-      
-      const deleteRes = await backendFetch(`/devices/${selectedDevice.id}`, 'DELETE', jwt);
+
+      const deleteRes = await backendFetch(
+        `/devices/${selectedDevice.id}`,
+        "DELETE",
+        jwt,
+      );
       if (!deleteRes.ok) {
         const err = await deleteRes.json();
-        throw new Error(err.message || 'Failed to delete the device.');
+        throw new Error(err.message || "Failed to delete the device.");
       }
       toast.success(`Device ${selectedDevice.name} has been deleted.`);
       await loadDevices();
     } catch (e) {
       console.error("Error deleting device:", e);
       toast.error("Deletion Failed", {
-        description: (e as Error).message
+        description: (e as Error).message,
       });
     } finally {
       setIsSubmitting(false);
@@ -273,7 +287,10 @@ export default function DevicesPage() {
     }
   };
 
-  const memoizedColumns = useMemo(() => getDeviceColumns(handleNameClick), [handleNameClick]);
+  const memoizedColumns = useMemo(
+    () => getDeviceColumns(handleNameClick),
+    [handleNameClick],
+  );
 
   if (initialLoading || !user) return <div></div>;
 
@@ -284,109 +301,129 @@ export default function DevicesPage() {
           <p className="text-2xl font-michroma">Devices</p>
           <div>
             <DataTable
-              columns={memoizedColumns} 
-              data={devices} 
-              filterColumnKey="name" 
+              columns={memoizedColumns}
+              data={devices}
+              filterColumnKey="name"
               option={
                 <div className="flex gap-4">
-                  <AddDeviceButton/>
-                  <ReloadButton onClick={loadDevices} isLoading={isDevicesLoading}/>
-                </div>
-                }
-              />
-            </div>
-          </div>
-        </div>
-
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogContent className="sm:max-w-md bg-[#1A2841] border-slate-700 border">
-            <DialogHeader>
-              <DialogTitle>Edit Device</DialogTitle>
-              <DialogDescription className="text-white">
-                {selectedDevice?.userId === user.$id ? "You can change the device name." : "You can only view details for devices not registered by you."}
-              </DialogDescription>
-            </DialogHeader>
-            {selectedDevice && (
-              <div className="space-y-4 py-2 text-sm">
-                <div className="flex items-center justify-between gap-4">
-                  <label 
-                    htmlFor="deviceName-edit" 
-                    className="flex-shrink-0 text-white text-muted-foreground"
-                  >
-                    Device Name:
-                  </label>
-                  <Input
-                    id="deviceName-edit"
-                    value={editedName}
-                    onChange={(e) => setEditedName(e.target.value)}
-                    disabled={selectedDevice.userId !== user.$id || isSubmitting}
-                    className="text-right text-sm font-medium max-w-50" // 다른 값들과 정렬을 맞추기 위해 text-right 추가
+                  <AddDeviceButton />
+                  <ReloadButton
+                    onClick={loadDevices}
+                    isLoading={isDevicesLoading}
                   />
                 </div>
-                <div className="flex items-center justify-between border-t pt-4">
-                  <p className="text-muted-foreground text-white">User:</p>
-                  <p className="font-medium truncate" title={selectedDevice.userEmail}>
-                    {selectedDevice.userEmail}
-                    </p>
-                </div>
-                <div className="flex items-center justify-between border-t pt-4">
-                  <p className="text-muted-foreground text-white">Assigned Subnet:</p>
-                  <p className="font-mono font-medium">{selectedDevice.subnet}</p>
-                </div>
+              }
+            />
+          </div>
+        </div>
+      </div>
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="sm:max-w-md bg-[#1A2841] border-slate-700 border">
+          <DialogHeader>
+            <DialogTitle>Edit Device</DialogTitle>
+            <DialogDescription className="text-white">
+              {selectedDevice?.userId === user.$id
+                ? "You can change the device name."
+                : "You can only view details for devices not registered by you."}
+            </DialogDescription>
+          </DialogHeader>
+          {selectedDevice && (
+            <div className="space-y-4 py-2 text-sm">
+              <div className="flex items-center justify-between gap-4">
+                <label
+                  htmlFor="deviceName-edit"
+                  className="flex-shrink-0 text-white text-muted-foreground"
+                >
+                  Device Name:
+                </label>
+                <Input
+                  id="deviceName-edit"
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                  disabled={selectedDevice.userId !== user.$id || isSubmitting}
+                  className="text-right text-sm font-medium max-w-50" // 다른 값들과 정렬을 맞추기 위해 text-right 추가
+                />
+              </div>
+              <div className="flex items-center justify-between border-t pt-4">
+                <p className="text-muted-foreground text-white">User:</p>
+                <p
+                  className="font-medium truncate"
+                  title={selectedDevice.userEmail}
+                >
+                  {selectedDevice.userEmail}
+                </p>
+              </div>
+              <div className="flex items-center justify-between border-t pt-4">
+                <p className="text-muted-foreground text-white">
+                  Assigned Subnet:
+                </p>
+                <p className="font-mono font-medium">{selectedDevice.subnet}</p>
+              </div>
+            </div>
+          )}
+          <DialogFooter>
+            <DialogClose
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                "cursor-pointer",
+              )}
+            >
+              Cancel
+            </DialogClose>
+            {selectedDevice?.userId === user.$id && (
+              <div className="flex gap-2">
+                <AlertDialog>
+                  <AlertDialogTrigger
+                    className={cn(
+                      buttonVariants({ variant: "destructive" }),
+                      "cursor-pointer",
+                    )}
+                    disabled={isSubmitting}
+                  >
+                    Delete
+                  </AlertDialogTrigger>
+                  <AlertDialogContent className="bg-[#1A2841] border-slate-700 border">
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription className="text-white">
+                        This action cannot be undone. This will permanently
+                        delete the device{" "}
+                        <span className="font-semibold text-red-500">
+                          {selectedDevice?.name}
+                        </span>{" "}
+                        and any associated containers.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel className="cursor-pointer bg-[#1A2841]">
+                        Cancel
+                      </AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={handleDelete}
+                        disabled={isSubmitting}
+                        className="cursor-pointer"
+                      >
+                        Continue
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+                <Button
+                  className="cursor-pointer"
+                  onClick={handleSave}
+                  disabled={isSubmitting}
+                >
+                  Save Changes
+                </Button>
               </div>
             )}
-            <DialogFooter>
-              <DialogClose 
-                className={cn(
-                buttonVariants({ variant: "ghost" }),
-                "cursor-pointer"
-                )}
-              >
-                Cancel
-              </DialogClose>
-              {selectedDevice?.userId === user.$id && (
-                <div className="flex gap-2">
-                  <AlertDialog>
-                    <AlertDialogTrigger
-                      className={cn( 
-                        buttonVariants({ variant: "destructive" }), 
-                        "cursor-pointer" 
-                      )}
-                      disabled={isSubmitting}
-                    >
-                      Delete
-                    </AlertDialogTrigger>
-                    <AlertDialogContent className="bg-[#1A2841] border-slate-700 border">
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                        <AlertDialogDescription className="text-white">
-                          This action cannot be undone. This will permanently delete the device 
-                          {' '}
-                          <span className="font-semibold text-red-500">{selectedDevice?.name}</span>
-                          {' '}
-                          and any associated containers.
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter>
-                        <AlertDialogCancel className="cursor-pointer bg-[#1A2841]">Cancel</AlertDialogCancel>
-                        <AlertDialogAction
-                          onClick={handleDelete}
-                          disabled={isSubmitting}
-                          className="cursor-pointer"
-                        >
-                          Continue
-                        </AlertDialogAction>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                  <Button className="cursor-pointer" onClick={handleSave} disabled={isSubmitting}>
-                    Save Changes
-                  </Button>
-                </div>
-              )}
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </MainLayout>
-    );
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </MainLayout>
+  );
 }
+

f6876fd982efd90f73fe8e869b1e5a0431e1b66e
Marc Owen <marcowen20@gmail.com>
2025-06-13 13:25:55 +0900

    Remove extra WsDeviceStatusPayload definition

--- coordination_server/src/events/ws-device-status-payload.dto.ts
+++ coordination_server/src/events/ws-device-status-payload.dto.ts
@@ -8,12 +8,3 @@ class WsContainerEvent {
     Object.assign(this, partial);
   }
 }
-
-export class WsDeviceStatusPayload {
-  readonly token: string;
-  readonly containers: WsContainerEvent[];
-
-  constructor(partial: Partial<WsDeviceStatusPayload>) {
-    Object.assign(this, partial);
-  }
-}
